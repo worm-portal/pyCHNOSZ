de98488816baa917cd53ee269d5e4913d271ca1e
Grayson Boyer <gmboyer@asu.edu>
2025-06-18 22:29:42 +0000

    commit before packaging for pypi

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyCHNOSZ",
-    version="0.8.16",
+    version="0.9.0",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Python wrapper for the R package CHNOSZ.",
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.10',
-    install_requires=['rpy2', 'pandas', 'ipython', 'plotly', 'simplegeneric', 'chemparse', 'WORMutils', 'matplotlib', 'numpy', 'wormutils_r'],
+    install_requires=['rpy2', 'pandas', 'ipython', 'plotly', 'simplegeneric', 'chemparse', 'WORMutils', 'matplotlib', 'numpy', 'wormutils_r', 'decimal', 'statistics'],
     package_data={'': ['*.r']},
     include_package_data=True,
     zip_safe=False
